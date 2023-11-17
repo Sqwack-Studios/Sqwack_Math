6d0b6a77e9d9dd794fdc83800e73649513757355
wildpaco <transistordaniel10@gmail.com>
2023-11-17 18:55:01 +0100

    Added TODOs

--- premake5.lua
+++ premake5.lua
@@ -7,7 +7,21 @@ workspace "sqwack_math"
 		"Release"
 	}
 
-	
+startproject "Sandbox"
+
+
+filter "configurations:Debug"
+    defines "_DEBUG"
+    symbols "on"
+
+filter "configurations:Release"
+    defines "_RELEASE"
+    optimize "Speed"
+    symbols "off"
+
+warnings "Everything"
+defines "SQ_MATH_SIMD"
+
 	
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
@@ -16,7 +30,7 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 project "sqwack_math"
 
 	location "sqwack_math"
-	kind "ConsoleApp"
+	kind "StaticLib"
 	language "C++"
 	cppdialect "C++20"
 
@@ -24,10 +38,6 @@ project "sqwack_math"
 	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
 	debugdir ("bin/" .. outputdir .. "/%{prj.name}")
 
-
-	warnings "Everything"
-    defines "SQ_MATH_SIMD"
-	
 	files
 	{
 		"%{prj.name}/**.hpp",
@@ -45,19 +55,57 @@ project "sqwack_math"
 	}
 
 
-		filter "system:windows"
+	    filter "system:windows"
 
-		systemversion "latest"
-		staticruntime "on"
-		flags {"MultiProcessorCompile"}
+	    systemversion "latest"
+	    staticruntime "on"
+	    flags {"MultiProcessorCompile"}
 
 
 
-		filter "configurations:Debug"
-			defines "_DEBUG"
-			symbols "on"
 
-		filter "configurations:Release"
-		    defines "_RELEASE"
-		    optimize "Speed"
-			symbols "off"
+project "Sandbox"
+
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+	debugdir ("bin/" .. outputdir .. "/%{prj.name}")
+
+
+
+	
+	files
+	{
+		"%{prj.name}/**.hpp",
+		"%{prj.name}/**.hh",
+		"%{prj.name}/**.hxx",
+		"%{prj.name}/**.h",
+
+		"%{prj.name}/**.cpp",
+		"%{prj.name}/**.c"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/include"
+	}
+
+	links 
+	{
+		sqwack_math
+	}
+
+	    filter "system:windows"
+
+	    systemversion "latest"
+	    staticruntime "on"
+	    flags {"MultiProcessorCompile"}
+
+
+
+
+
---
+++ sqwack_math/TODOs.hpp
@@ -0,0 +1,86 @@
+#pragma once
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//																																				  //
+//     ******This file contains all the work left to do in the math library. It will also contain all the requirements for the library*******	  //
+// 																																				  //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                          																					  //
+//                                                         *** Main Requirements / Design ***													  //
+//																																				  //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//																																				  //
+//           a) The library must support vector2, vector3, vector4 and matrix types from 2x2 up to 4x4, included non-square sizes				  //
+// 																																				  //
+//           b) For now, it will only support float types for computations.																		  //
+// 																																				  //
+//           c) Vector and matrix types will be renamed following HLSL types: Vector2 -> float2, Vector3 -> float3...							  //
+//                                                                            Matrix2x2 -> float2x2, Matrix3x3 -> float3x3...					  //
+// 																																				  //
+//           d) Containers must be templated so that someone can define their own Vector/Matrix types. This base class will be extremely simple.  //
+// 																																				  //
+//             - d.1) Mathematical types will be specialized template definitions containing mathematical functions								  //
+// 																																				  //
+//           e) Support for component swizzle																									  //
+// 																																				  //
+//           f) SIMD instructions supported																										  //
+// 																																				  //
+//             - f.1) Provide a SIMD type and expose SIMD operations so that user can concatenate operations over the same type					  //
+//               , reducing number of loads/unloads.																							  //
+// 																																				  //
+//           g) Support common operations in vector and matrices, as well as some common helper functions in computer graphics, like rotations,   //
+//              																																  //
+//              lookAt matrix generation, handiness...																							  //
+//          																																	  //
+// 																																				  //
+//           e) Transform																														  //
+// 																																				  //
+// 																																				  //
+// 																																				  //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                     																											  //
+//                                                        *** Once Main Requirements are met ***												  //
+//                    																															  //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////          																																  //
+//                                                                                                                                                //
+//            a) Plane																															  //
+// 																																				  //
+//            b) Sphere																															  //
+// 																																				  //
+//            c) Triangle																														  //
+// 																																				  //
+//            d) Cube, AABB...																													  //
+// 																																				  //
+//            e) Frustum																														  //
+// 																																				  //
+//            f) Ray																															  //
+//             																																	  //
+//              - f.1) Ray - Intersection algorithms																							  //
+// 																																				  //
+// 																																				  //
+//            g.2) Explore AVX-512 types: provide an interface that takes advantage of SoA. Vector/Matrix operations can be batched 			  //
+//                 in single AVX instructions, for example, an AVX-512 register is 16 float wide.												  //
+//                 You can compute up to four float4 operations, five float3 operations, and 8 float2 operations in								  //
+//                 the same instruction. I think that this can heavily improve computation-extensive algorithms like physics engines			  //
+//																																				  //
+//																																				  //
+//																																				  //
+//																																				  //
+//																																				  //
+//																																				  //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////																																			  //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
